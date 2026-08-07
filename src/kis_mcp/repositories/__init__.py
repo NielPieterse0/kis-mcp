a14995e94fffa935d5711f1267f5ac54218a6b46
NielPieterse0 <222640156+NielPieterse0@users.noreply.github.com>
2026-08-07 07:39:11 +0200

    feat: expose repository settings contracts

---
+++ src/kis_mcp/repositories/__init__.py
@@ -0,0 +1,15 @@
+from .settings import (
+    GitHubProjectBinding,
+    RepositorySettings,
+    SelectedRepositorySettings,
+    load_repository_settings,
+    normalize_github_repository,
+)
+
+__all__ = [
+    "GitHubProjectBinding",
+    "RepositorySettings",
+    "SelectedRepositorySettings",
+    "load_repository_settings",
+    "normalize_github_repository",
+]
